5d3d5f0c2ac31d699fb39b4acf103d1125ed5504
Natan Souza <42328139+natan1506@users.noreply.github.com>
2023-10-10 11:34:40 -0300

    Feat: defaultopen

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -14,10 +14,15 @@ interface ModuleProps {
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   const dispatch = useDispatch()
 
-  const lessons = useAppSelector(state => state.player.course.modules[moduleIndex].lessons)
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+    return { currentModuleIndex, currentLessonIndex }
+  })
+
+  const lessons = useAppSelector((state) => state.player.course.modules[moduleIndex].lessons)
 
   return (
-    <Collapsible.Root className="group">
+    <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
       <Collapsible.Trigger className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
         <div className='flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm'>{moduleIndex + 1}</div>
 
@@ -33,14 +38,19 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className='relative flex flex-col gap-4 p-6'>
-          {lessons.map((lesson, lessonIndex) => (
-            <Lesson
-              key={lesson.id}
-              title={lesson.title}
-              duration={lesson.duration}
-              onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-            />
-          ))}
+          {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
+            return (
+              <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
+              />
+            )
+          })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
