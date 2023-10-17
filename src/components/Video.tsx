e7b58bb2a7a9c6ceb93b45adffe3c1d1d198a949
Natan Souza <42328139+natan1506@users.noreply.github.com>
2023-10-17 09:06:57 -0300

    Feat: initial unity test

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,13 @@
 import ReactPlayer from "react-player";
-import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
   const dispatch = useDispatch()
-  const lesson = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handlePlayNext() {
-    dispatch(next(''))
+    dispatch(next())
   }
 
   return (
@@ -25,7 +18,7 @@ export function Video() {
         controls
         playing
         onEnded={handlePlayNext}
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   )
